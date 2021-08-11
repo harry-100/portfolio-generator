780bdf63554e5a871db5c9370f4e2bb3c69727a9
Harry Shah <harvinder.shah@gmail.com>
2021-08-11 17:12:59 -0400

    code refactored

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 
@@ -131,34 +131,18 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    
-    fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw err;
-    console.log("portfolio complete! Check out the index.html to see the output");
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
-    
-});
-
-// Mock data alternative 
-// const mockData = {
-//   name: 'harry',
-//   github: 'harry-2',
-//   confirmAbout: true,
-//   about: 'sdfsdfdsf sdffsdf sdfdsfsdf sdfsdfdsf',
-//   projects: [
-//     {
-//       name: 'proj-1',
-//       description: 'ffdfds',
-//       languages: ['HTML', 'CSS', 'javaScript'],
-//       feature: true,
-//       confirmAddProject: false
-//     }
-//   ]
-// }
-
-// const pageHTML = generatePage(portfolioData);
-// fs.writeFile('index.html', pageHTML, err => {
-// if (err) throw err;
-// console.log("portfolio complete! Check out the index.html to see the output");
-// });
